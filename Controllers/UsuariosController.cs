7a5b8b934f21e0db484b7c700289ab3fdecaf347
Vinicius Sampaio <vinicius_travessa@hotmail.com>
2022-05-26 22:36:43 -0300

    Segunda

--- Controllers/UsuariosController.cs
+++ Controllers/UsuariosController.cs
@@ -6,6 +6,7 @@ using System.Net.Http.Headers;
 using Microsoft.AspNetCore.Http;
 using Newtonsoft.Json;
 
+
 namespace RpgMvc.Controllers
 {
     public class UsuariosController : Controller
@@ -57,6 +58,39 @@ namespace RpgMvc.Controllers
         {
             return View("AutenticarUsuario");
         }
+        [HttpPost]
+        public async Task<ActionResult> AutenticarAsync(UsuarioViewModel u)
+        {
+            try
+            {
+                HttpClient httpClient = new HttpClient();
+                string uriComplementar = "Autenticar";
+
+                var content = new StringContent(JsonConvert.SerializeObject(u));
+                content.Headers.ContentType = new MediaTypeHeaderValue("application/json");
+                HttpResponseMessage response = await httpClient.PostAsync(uriBase + uriComplementar, content);
+
+                string serialized = await response.Content.ReadAsStringAsync();
+
+                if (response.StatusCode == System.Net.HttpStatusCode.OK)
+                {
+                    HttpContext.Session.SetString("SessionTokenUsuario", serialized);
+                    TempData["Mensagem"] = string.Format("Bem-vindo {0} !!!", u.Username);
+                    return RedirectToAction("Index", "Personagens");    
+                }
+                else
+                {
+                    throw new System.Exception(serialized);
+                }
+
+            }
+            catch (System.Exception ex)
+            {
+                TempData["MensagemErro"] = ex.Message;
+                return IndexLogin();
+            }
+        }
+       
         
     }
 }
