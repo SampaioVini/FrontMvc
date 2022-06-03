505044a3d110965966a3720a6f728d5c3daf5b11
Vinicius Sampaio <vinicius_travessa@hotmail.com>
2022-06-03 16:55:32 -0300

    Caminhando

--- Controllers/DisputasController.cs
+++ Controllers/DisputasController.cs
@@ -44,5 +44,86 @@ namespace RpgMvc.Controllers
                 return RedirectToAction("Index");
             }
         }
+        [HttpPost]
+        public async Task<ActionResult> IndexAsync(DisputaViewModel disputa)
+        {
+            try
+            {
+                HttpClient httpClient = new HttpClient();
+                string uriComplementar = "Arma";
+                
+                var content = new StringContent(JsonConvert.SerializeObject(disputa));
+                content.Headers.ContentType = new MediaTypeHeaderValue("application/json");
+                HttpResponseMessage response = await httpClient.PostAsync(uriBase + uriComplementar, content); 
+                string serialized = await response.Content.ReadAsStringAsync();
+
+                if (response.StatusCode == System.Net.HttpStatusCode.OK)
+                {
+                    disputa = await Task.Run(() => JsonConvert.DeserializeObject<DisputaViewModel>(serialized));
+                     TempData["Mensagem"] = disputa.Narracao;
+                    return RedirectToAction("Index", "Personagens");
+                }
+                else
+                    throw new System.Exception(serialized);
+
+            }
+            catch (System.Exception ex)
+            {
+                TempData["MensagemErro"] = ex.Message;
+                return RedirectToAction("Index");
+            }
+    
+        }
+        
+        [HttpGet]
+        public async Task<ActionResult> IndexHabilidadeAsync()
+        {
+            try
+            {
+                
+               HttpClient httpClient = new HttpClient();
+                string token = HttpContext.Session.GetString("SessionTokenUsuario");
+                httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+
+                string uriBuscaPersonagens = "http://localhost:5000/Personagens/GetAll";
+                HttpResponseMessage response = await httpClient.GetAsync(uriBuscaPersonagens); 
+                string serialized = await response.Content.ReadAsStringAsync();
+
+                if (response.StatusCode == System.Net.HttpStatusCode.OK)
+                {
+                    List<PersonagemViewModel> listaPersonagens = await Task.Run(() =>
+                        JsonConvert.DeserializeObject<List<PersonagemViewModel>>(serialized));
+
+                    ViewBag.ListaAtacantes = listaPersonagens;
+                    ViewBag.ListaOponentes = listaPersonagens;
+                }
+                else
+                    throw new System.Exception(serialized);
+
+                string uriBuscaHabilidades = "http://localhost:5000/PersonagemHabilidades/GetHabilidades";   
+                response = await httpClient.GetAsync(uriBuscaHabilidades);
+                serialized = await response.Content.ReadAsStringAsync();
+
+                
+                  if (response.StatusCode == System.Net.HttpStatusCode.OK)
+                {
+                    List<HabilidadeViewModel> listaHabilidades = await Task.Run(() => 
+                        JsonConvert.DeserializeObject<List<HabilidadeViewModel>>(serialized));
+                    ViewBag.ListaHabilidades = listaHabilidades;
+                }
+                else
+                    throw new System.Exception(serialized);
+
+                return View ("IndexHabilidades");
+            
+
+            }
+            catch (System.Exception ex)
+            {
+                TempData["MensagemErro"] = ex.Message;
+                return RedirectToAction("Index");
+            }
+        }
+
     }
 }
